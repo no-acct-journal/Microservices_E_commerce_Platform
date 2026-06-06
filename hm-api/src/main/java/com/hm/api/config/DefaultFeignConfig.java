02babb84a04d00322786d3205d2e3fbb28260e6e
no-acct-journal <arthurshen168@gmail.com>
2026-06-06 12:11:25 -0700

    interceptor

--- hm-api/src/main/java/com/hm/api/config/DefaultFeignConfig.java
+++ hm-api/src/main/java/com/hm/api/config/DefaultFeignConfig.java
@@ -1,6 +1,9 @@
 package com.hm.api.config;
 
+import com.hmall.common.utils.UserContext;
 import feign.Logger;
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
 import org.springframework.context.annotation.Bean;
 
 public class DefaultFeignConfig {
@@ -8,4 +11,23 @@ public class DefaultFeignConfig {
     public Logger.Level feignLogLevel(){
         return Logger.Level.FULL;
     }
+
+    @Bean
+    public RequestInterceptor userInfoRequestInterceptor(){
+        return new RequestInterceptor() {
+            @Override
+            public void apply(RequestTemplate template) {
+                // 获取登录用户
+                Long userId = UserContext.getUser();
+                if(userId == null) {
+                    // 如果为空则直接跳过
+                    return;
+                }
+                // 如果不为空则放入请求头中，传递给下游微服务
+                template.header("user-info", userId.toString());
+            }
+        };
+    }
 }
+
+
